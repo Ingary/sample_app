86b37cac26b80c353f626f193bc9f7141b2ccf2c
Ingary <tikymikk@gmail.com>
2013-11-06 16:53:06 -0600

    Finish user signup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-
+  #By default, methods defined in any helper file are automatically available in any view.
   # Returns the full title on a per-page basis.
   def full_title(page_title)
     base_title = "Ruby on Rails Tutorial Sample App"
