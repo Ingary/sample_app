e172c9340ea605ec4b5380492aa1101cd50cfbe0
Ingary <tikymikk@gmail.com>
2013-11-10 21:27:41 -0600

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,9 +8,14 @@ class SessionsController < ApplicationController
   	  if user && user.authenticate(params[:session][:password])
         # Sign the user in and redirect to the user's show page.
         sign_in user
-        redirect_to user
+        redirect_back_or user
   	  else
         flash.now[:error] = 'Invalid email/password combination'
+        #The issue is that the contents of the flash persist for one request, but—unlike a 
+        #redirect, re-rendering a template with render doesn’t count as a request.
+        #To get the failing test to pass, instead of flash we use flash.now, which is 
+        #specifically designed for displaying flash messages on rendered pages; unlike the 
+        #contents of flash, its contents disappear as soon as there is an additional request. 
         render 'new'
   	  end
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,9 +16,21 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
 
   it { should be_valid }
+  it { should_not be_admin }
 
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+      #toggle! cambia el atributo admin de false a true.
+    end
+
+    it { should be_admin }
+    #implies (via the RSpec boolean convention) that the user should have an admin? boolean method.
+  end
 
   describe "when name is not present" do
     before { @user.name = " " }
